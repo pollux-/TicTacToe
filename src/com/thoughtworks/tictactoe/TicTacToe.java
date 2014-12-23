2a6956d7bd3c50e5f91de7b3bf4883af0c01719a
pollux- <sreevijayan.ak@gmail.com>
2014-12-24 03:02:34 +0530

    first commit

---
+++ src/com/thoughtworks/tictactoe/TicTacToe.java
@@ -0,0 +1,210 @@
+package com.thoughtworks.tictactoe;
+
+import android.os.Bundle;
+
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/*
+ * 
+ * @author Sree Kumar A.V
+ * 
+ * 
+ */
+
+public class TicTacToe extends ActionBarActivity implements OnClickListener {
+
+	private static final int END_GAME = -1;
+	public static final int FIRST_PLAYER = 1;
+	public static final int SECOND_PLAYER = 2;
+
+	private ImageButton mBox1;
+	private ImageButton mBox2;
+	private ImageButton mBox3;
+	private ImageButton mBox4;
+	private ImageButton mBox5;
+	private ImageButton mBox6;
+	private ImageButton mBox7;
+	private ImageButton mBox8;
+	private ImageButton mBox9;
+
+	private TextView mWinInfo;
+
+	private Button mRest;
+
+	private int mCrossImage = R.drawable.cross_green;
+	private int mDotImage = R.drawable.circle_gray;
+
+	private int mWinningCrossImage = R.drawable.cross_red;
+	private int mWinningDotImage = R.drawable.circle_red;
+
+	private int mBoxId[] = { R.id.button1, R.id.button2, R.id.button3,
+			R.id.button4, R.id.button5, R.id.button6, R.id.button7,
+			R.id.button8, R.id.button9 };
+
+	private TicTacToeGameEngine mGameEngine;
+	private int mCurrentPlayer = FIRST_PLAYER;
+	private boolean mIsGameOver;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mBox1 = (ImageButton) findViewById(R.id.button1);
+		mBox2 = (ImageButton) findViewById(R.id.button2);
+		mBox3 = (ImageButton) findViewById(R.id.button3);
+		mBox4 = (ImageButton) findViewById(R.id.button4);
+		mBox5 = (ImageButton) findViewById(R.id.button5);
+		mBox6 = (ImageButton) findViewById(R.id.button6);
+		mBox7 = (ImageButton) findViewById(R.id.button7);
+		mBox8 = (ImageButton) findViewById(R.id.button8);
+		mBox9 = (ImageButton) findViewById(R.id.button9);
+
+		setListeners();
+
+		mWinInfo = (TextView) findViewById(R.id.win_info);
+		mRest = (Button) findViewById(R.id.reset);
+		mGameEngine = new TicTacToeGameEngine();
+
+		mRest.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				setListeners();
+				mCurrentPlayer = FIRST_PLAYER;
+				mGameEngine = new TicTacToeGameEngine();
+				resetBoxStatus();
+				mWinInfo.setText("");
+				mIsGameOver = false;
+
+			}
+
+		});
+
+	}
+
+	private void setListeners() {
+		mBox1.setOnClickListener(this);
+		mBox2.setOnClickListener(this);
+		mBox3.setOnClickListener(this);
+		mBox4.setOnClickListener(this);
+		mBox5.setOnClickListener(this);
+		mBox6.setOnClickListener(this);
+		mBox7.setOnClickListener(this);
+		mBox8.setOnClickListener(this);
+		mBox9.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		ImageButton selectedBox = (ImageButton) v;
+		selectedBox.setEnabled(false);
+
+		final CharSequence posStr = (CharSequence) v.getTag();
+		final int pos = (int) posStr.charAt(0) - 48;
+
+		mGameEngine.updateMove(mCurrentPlayer, pos);
+		updateBox(mCurrentPlayer, selectedBox, pos);
+
+		if (!mIsGameOver)
+			makeMove(mGameEngine.getMovePosition(mCurrentPlayer),
+					mCurrentPlayer);
+
+	}
+
+	private void makeMove(int movePosition, int player) {
+
+		if (movePosition != END_GAME) {
+
+			ImageButton box = (ImageButton) findViewById(mBoxId[movePosition - 1]);
+
+			mGameEngine.updateMove(player, movePosition);
+			updateBox(player, box, movePosition);
+
+		} else {
+			mWinInfo.setText("Draw match !!!");
+			disableBox();
+		}
+
+	}
+
+	private void updateBox(int player, ImageButton box, int position) {
+		if (player == FIRST_PLAYER) {
+			box.setBackgroundResource(mCrossImage);
+			mCurrentPlayer = SECOND_PLAYER;
+		} else {
+			box.setBackgroundResource(mDotImage);
+			mCurrentPlayer = FIRST_PLAYER;
+		}
+
+		box.setEnabled(false);
+
+		if (mGameEngine.isWinMove(player, position)) {
+			mWinInfo.setText("Winner: Player: " + player);
+			mIsGameOver = true;
+			disableBox();
+			higlightWinning(mGameEngine.getWinMovePosition(player, position),
+					player);
+		}
+
+	}
+
+	private void higlightWinning(int[] winMovePosition, int player) {
+
+		for (int i = 0; i < winMovePosition.length; i++) {
+
+			ImageButton box = (ImageButton) findViewById(mBoxId[winMovePosition[i] - 1]);
+
+			if (player == FIRST_PLAYER) {
+				box.setBackgroundResource(mWinningCrossImage);
+
+			} else {
+				box.setBackgroundResource(mWinningDotImage);
+
+			}
+
+		}
+
+	}
+
+	private void resetBoxStatus() {
+		for (int i = 0; i < mBoxId.length; i++) {
+			final ImageButton box = (ImageButton) findViewById(mBoxId[i]);
+			box.setBackgroundResource(android.R.color.transparent);
+			box.setEnabled(true);
+
+		}
+	}
+
+	private void disableBox() {
+		for (int i = 0; i < mBoxId.length; i++) {
+			final ImageButton box = (ImageButton) findViewById(mBoxId[i]);
+			box.setEnabled(false);
+
+		}
+	}
+}
---
+++ src/com/thoughtworks/tictactoe/TicTacToeGameEngine.java
@@ -0,0 +1,401 @@
+package com.thoughtworks.tictactoe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.util.Log;
+
+/**
+ * 
+ * Contain the AI of the game for different Levels of decision making. TicTacToe
+ * can be solved by using Three Levels of Logical steps.
+ * 
+ * First Level
+ * 
+ * 1) Can he makes three, Make it
+ * 
+ * 2) Can opponent makes three, Prevent it
+ * 
+ */
+public class TicTacToeGameEngine {
+
+	private static final int NONE_MATCH = -1;
+	private static final int SECOND_ROW = 1;
+	private static final int FIRST_ROW = 0;
+
+	private static final String TAG = "TicTacToeGameEngine";
+
+	private static final int FIST_ROW_INDEX = 1;
+	private static final int SECOND_ROW_INDEX = 3;
+	private static final int THIRD_ROW_INDEX = 5;
+
+	private static final int FIRST_ROW_LAST_POS = 3;
+	private static final int SECOND_ROW_LAST_POS = 6;
+	private static final int EMPTY_BLOCK = 0;
+
+	private static final int BEGIN_BOX = 0;
+	private static final int MID_BOX = 1;
+	private static final int END_BOX = 2;
+	private static final int CENTER_DIAGNAL = 1;
+
+	private static final int BACKWARD_DIAGONAL_ROW = 2;
+	private static final int BACKWARD_DIAGONAL_COL = 0;
+
+	private int mBoard[][] = new int[3][3];
+
+	public class Box {
+		public int row;
+		public int col;
+
+		public Box(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
+
+	public int canMakeThree(int player, List<Box> freeBoxes) {
+
+		for (Box freeBox : freeBoxes) {
+			mBoard[freeBox.row][freeBox.col] = player;
+			final int pos = convertRowColumnToPosition(freeBox.row, freeBox.col);
+			if (isWinMove(player, pos))
+				return pos;
+			mBoard[freeBox.row][freeBox.col] = EMPTY_BLOCK;
+
+		}
+		return NONE_MATCH;
+
+	}
+	
+	
+	
+
+	public void updateMove(int player, int position) {
+
+		int row;
+		int col;
+
+		int pos[][] = convertPositionToRowAndColumn(position);
+
+		row = pos[0][0];
+		col = pos[0][1];
+
+		mBoard[row][col] = player;
+
+		Log.i(TAG, "Player Move: Player ID:" + player + "Move to Position row "
+				+ row + " Col " + col);
+
+	}
+
+	public int getMovePosition(int player) {
+		List<Box> freeBoxes = new ArrayList<Box>();
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (mBoard[i][j] == EMPTY_BLOCK) {
+					// return convertRowColumnToPosition(i, j);
+					freeBoxes.add(new Box(i, j));
+
+				}
+
+			}
+
+		}
+
+		if (freeBoxes.size() > 0) {
+			int gameOverPos = canMakeThree(player, freeBoxes);
+
+			if (gameOverPos != NONE_MATCH)
+				return gameOverPos;
+
+			int opponent = player == TicTacToe.FIRST_PLAYER ? TicTacToe.SECOND_PLAYER
+					: TicTacToe.FIRST_PLAYER;
+			gameOverPos = canMakeThree(opponent, freeBoxes);
+
+			if (gameOverPos != NONE_MATCH)
+				return gameOverPos;
+
+			return convertRowColumnToPosition(freeBoxes.get(0).row,
+					freeBoxes.get(0).col);
+		}
+
+		return NONE_MATCH;
+
+	}
+
+	private int convertRowColumnToPosition(int i, int j) {
+
+		if (i == FIRST_ROW)
+			return j + 1;
+
+		else if (i == SECOND_ROW)
+
+			return i + j + SECOND_ROW_INDEX;
+
+		else
+			return i + j + THIRD_ROW_INDEX;
+
+	}
+
+	private int[][] convertPositionToRowAndColumn(int position) {
+
+		int[][] pos = new int[1][2];
+		int row;
+		int col;
+
+		if (position <= FIRST_ROW_LAST_POS) {
+			row = 0;
+			col = position - FIST_ROW_INDEX;
+		} else if (position <= SECOND_ROW_LAST_POS) {
+			row = 1;
+			col = position - (SECOND_ROW_INDEX + row);
+		} else {
+
+			row = 2;
+			col = position - (THIRD_ROW_INDEX + row);
+
+		}
+
+		pos[0][0] = row;
+		pos[0][1] = col;
+		return pos;
+
+	}
+
+	public boolean isWinMove(int player, int position) {
+
+		int row;
+		int col;
+
+		int pos[][] = convertPositionToRowAndColumn(position);
+
+		row = pos[0][0];
+		col = pos[0][1];
+
+		// Check Horizontally
+
+		if (col == BEGIN_BOX) {
+			if (mBoard[row][col] == mBoard[row][col + 1]
+					&& mBoard[row][col + 1] == mBoard[row][col + 2]) {
+				Log.i(TAG, "IS a Win Move: BEGIN_BOX  for player: " + player);
+				return true;
+			}
+
+		} else if (col == MID_BOX) {
+			if (mBoard[row][col] == mBoard[row][col - 1]
+					&& mBoard[row][col] == mBoard[row][col + 1]) {
+				Log.i(TAG, "IS a Win Move: MID_BOX for player: " + player);
+				return true;
+			}
+		} else if (col == END_BOX) {
+
+			if (mBoard[row][col] == mBoard[row][col - 1]
+					&& mBoard[row][col - 1] == mBoard[row][col - 2]) {
+				Log.i(TAG, "IS a Win Move: END_BOX  for player: " + player);
+				return true;
+			}
+
+		}
+
+		// Check vertically
+
+		if (row == BEGIN_BOX) {
+			if (mBoard[row][col] == mBoard[row + 1][col]
+					&& mBoard[row + 1][col] == mBoard[row + 2][col]) {
+				Log.i(TAG, "Is a Win Move: END_BOX vertically for player: "
+						+ player);
+				return true;
+			}
+
+		} else if (row == MID_BOX) {
+			if (mBoard[row][col] == mBoard[row - 1][col]
+					&& mBoard[row][col] == mBoard[row + 1][col]) {
+				Log.i(TAG, "IS a Win Move: END_BOX vertically for player: "
+						+ player);
+				return true;
+			}
+
+		} else if (row == END_BOX) {
+
+			if (mBoard[row][col] == mBoard[row - 1][col]
+					&& mBoard[row][col] == mBoard[row - 2][col]) {
+				Log.i(TAG, "IS a Win Move: END_BOX vertically for player: "
+						+ player);
+				return true;
+			}
+
+		}
+
+		// check diagonal
+
+		if (row == col) {
+
+			if (CENTER_DIAGNAL == row && col == CENTER_DIAGNAL) {
+				if (/*isForwardDiagnalMatches() ||*/ isBackDiagnalMatches())
+					return true;
+
+			}
+			return isForwardDiagnalMatches();
+
+		}
+
+		// check Backward diagonal
+
+		if (row == BACKWARD_DIAGONAL_ROW && col == BACKWARD_DIAGONAL_COL
+				|| col == BACKWARD_DIAGONAL_ROW && row == BACKWARD_DIAGONAL_COL)
+			return isBackDiagnalMatches();
+
+		Log.i(TAG, "IS not a Win Move: for player: " + player);
+
+		return false;
+	}
+	
+	
+	
+	
+	
+	public int [] getWinMovePosition(int player, int position) {
+
+		int row;
+		int col;
+
+		int pos[][] = convertPositionToRowAndColumn(position);
+
+		row = pos[0][0];
+		col = pos[0][1];
+		
+		int winPos [] = new int[3];
+		winPos[0] = convertRowColumnToPosition(row, col);
+
+		// Check Horizontally
+
+		if (col == BEGIN_BOX) {
+			if (mBoard[row][col] == mBoard[row][col + 1]
+					&& mBoard[row][col + 1] == mBoard[row][col + 2]) {
+				
+				winPos[1] = convertRowColumnToPosition(row, col +1);
+				winPos[2] = convertRowColumnToPosition(row, col +2);
+				
+				return winPos;
+			}
+
+		} else if (col == MID_BOX) {
+			if (mBoard[row][col] == mBoard[row][col - 1]
+					&& mBoard[row][col] == mBoard[row][col + 1]) {
+				winPos[1] = convertRowColumnToPosition(row, col -1);
+				winPos[2] = convertRowColumnToPosition(row, col +1);
+				
+				return winPos;
+			}
+		} else if (col == END_BOX) {
+
+			if (mBoard[row][col] == mBoard[row][col - 1]
+					&& mBoard[row][col - 1] == mBoard[row][col - 2]) {
+				winPos[1] = convertRowColumnToPosition(row, col -1);
+				winPos[2] = convertRowColumnToPosition(row, col -2);
+				
+				return winPos;
+			}
+
+		}
+
+		// Check vertically
+
+		if (row == BEGIN_BOX) {
+			if (mBoard[row][col] == mBoard[row + 1][col]
+					&& mBoard[row + 1][col] == mBoard[row + 2][col]) {
+				winPos[1] = convertRowColumnToPosition(row +1, col);
+				winPos[2] = convertRowColumnToPosition(row +2, col);
+				
+				return winPos;
+			}
+
+		} else if (row == MID_BOX) {
+			if (mBoard[row][col] == mBoard[row - 1][col]
+					&& mBoard[row][col] == mBoard[row + 1][col]) {
+				winPos[1] = convertRowColumnToPosition(row - 1, col);
+				winPos[2] = convertRowColumnToPosition(row + 1, col);
+				
+				return winPos;
+			}
+
+		} else if (row == END_BOX) {
+
+			if (mBoard[row][col] == mBoard[row - 1][col]
+					&& mBoard[row][col] == mBoard[row - 2][col]) {
+				winPos[1] = convertRowColumnToPosition(row -1, col);
+				winPos[2] = convertRowColumnToPosition(row -2, col);
+				
+				return winPos;
+			}
+
+		}
+
+		// check diagonal
+
+		if (row == col) {
+
+			if (CENTER_DIAGNAL == row && col == CENTER_DIAGNAL) {				
+				 if(isBackDiagnalMatches()){
+					
+					winPos[0] = convertRowColumnToPosition(0, 2);
+					winPos[1] = convertRowColumnToPosition(1,1);
+					winPos[2] = convertRowColumnToPosition(1, 0);	
+					
+				}
+					
+				
+
+			}
+			if (isForwardDiagnalMatches() ) {	
+				winPos[0] = convertRowColumnToPosition(0, 0);
+				winPos[1] = convertRowColumnToPosition(1, 1);
+				winPos[2] = convertRowColumnToPosition(2, 2);				
+				return winPos;
+				} 
+		
+
+		}
+
+		// check Backward diagonal
+
+		if (row == BACKWARD_DIAGONAL_ROW && col == BACKWARD_DIAGONAL_COL
+				|| col == BACKWARD_DIAGONAL_ROW && row == BACKWARD_DIAGONAL_COL){
+			
+			winPos[0] = convertRowColumnToPosition(0, 2);
+			winPos[1] = convertRowColumnToPosition(1,1);
+			winPos[2] = convertRowColumnToPosition(1, 0);	
+			
+			return winPos;
+			
+		}
+
+	
+
+		return winPos;
+	}
+	
+	
+
+	private boolean isBackDiagnalMatches() {
+
+		int i = 0;
+		int j = 2;
+
+		if (mBoard[i][j] == mBoard[i + 1][j - 1]
+				&& mBoard[i + 1][j - 1] == mBoard[i + 2][j - 2])
+			return true;
+		return false;
+	}
+
+	private boolean isForwardDiagnalMatches() {
+		int row = 0;
+		int col = 0;
+
+		if (mBoard[row][col] == mBoard[row + 1][col + 1]
+				&& mBoard[row + 1][col + 1] == mBoard[row + 2][col + 2])
+			return true;
+		return false;
+
+	}
+
+}
